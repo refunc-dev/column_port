bd6cc6b36af9fec21d3b2f82de70a8b7751f6b94
kefujiwa <kefujiwa@student.42tokyo.jp>
2022-06-09 22:48:32 +0900

    feat: メールアドレス・パスワードによるログイン方法に変更

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,37 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView,
+    PasswordChangeDoneView, PasswordResetView,
+    PasswordResetDoneView, PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
+from accounts.views import SignUpView
+from accounts.forms import EmailAuthenticationForm
+
 from django.urls import path
-from django.views.generic import CreateView
+
+
+signup_view = SignUpView.as_view()
+login_view = LoginView.as_view(
+    form_class=EmailAuthenticationForm,
+    redirect_authenticated_user=True,
+    template_name='accounts/login.html'
+)
+logout_view = LogoutView.as_view()
+password_change_view = PasswordChangeView.as_view()
+password_change_done_view = PasswordChangeDoneView.as_view()
+password_reset_view = PasswordResetView.as_view()
+password_reset_done_view = PasswordResetView.as_view()
+password_reset_confirm_view = PasswordResetConfirmView.as_view()
+password_reset_complete_view = PasswordResetCompleteView.as_view()
 
 urlpatterns = [
-    path('signup/', CreateView.as_view(
-        template_name='accounts/signup.html',
-        form_class=UserCreationForm,
-        success_url='/',
-    ), name='signup'),
-    path('login/', LoginView.as_view(
-        redirect_authenticated_user=True,
-        template_name='accounts/login.html'
-        ), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('password_change/', password_change_view, name='password_change'),
+    path('password_change/done/', password_change_done_view, name='password_change_done'),
+    path('password_reset/', password_reset_view, name='password_reset'),
+    path('password_reset/done/', password_reset_done_view, name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', password_reset_confirm_view, name='password_reset_confirm'),
+    path('reset/done/', password_reset_complete_view, name='password_reset_complete'),
 ]
