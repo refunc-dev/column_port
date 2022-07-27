2b8af2491ca78cbcffba4cdc077545805c3964b7
kefujiwa <kefujiwa@student.42tokyo.jp>
2022-07-28 01:41:17 +0900

    fix: カテゴリを必須から任意項目に変更

--- articles/forms.py
+++ articles/forms.py
@@ -4,12 +4,16 @@ from articles.models import Article
 
 
 class UpdateArticleForm(forms.ModelForm):
+    category = forms.CharField(max_length=100, required=False)
+
     class Meta:
         model = Article
         fields = ('project', 'path', 'title', 'published_at', 'updated_at', 'person_in_charge', 'category')
 
 
 class AddArticleForm(forms.ModelForm):
+    category = forms.CharField(max_length=100, required=False)
+
     class Meta:
         model = Article
         fields = ('path', 'title', 'published_at', 'updated_at', 'person_in_charge', 'category')
