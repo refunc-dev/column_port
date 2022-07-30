8cb5f8665c2e21153f01e496c4cd03e21dbc3247
kefujiwa <kefujiwa@student.42tokyo.jp>
2022-07-30 14:13:10 +0900

    fix: 設定に認証機能のデフォルト設定を追加

--- api/views.py
+++ api/views.py
@@ -14,11 +14,6 @@ import re
 class KeywordListAPIView(generics.ListAPIView):
     queryset = Keyword.objects.all()
     serializer_class = KeywordListSerializer
-    authentication_classes = [
-        authentication.SessionAuthentication,
-        authentication.TokenAuthentication
-    ]
-    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
@@ -28,21 +23,11 @@ class KeywordListAPIView(generics.ListAPIView):
 class KeywordUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = Keyword.objects.all()
     serializer_class = KeywordUpdateSerializer
-    authentication_classes = [
-        authentication.SessionAuthentication,
-        authentication.TokenAuthentication
-    ]
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class KeywordSerpCreateAPIView(generics.CreateAPIView):
     queryset = KeywordSerp.objects.all()
     serializer_class = KeywordSerpSerializer
-    authentication_classes = [
-        authentication.SessionAuthentication,
-        authentication.TokenAuthentication
-    ]
-    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serps = serializer.save()
--- column_port/settings.py
+++ column_port/settings.py
@@ -212,6 +212,13 @@ EMAIL_USE_TLS = True
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
